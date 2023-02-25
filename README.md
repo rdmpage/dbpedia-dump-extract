b8afd06cc251bd023846351a60ffe6eba2d5ebe7
Roderic Page <rdmpage@gmail.com>
2023-02-25 15:51:23 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# dbpedia-dump-extract
-Extract relevant triples from DBPedia dumps
+# DBPedia Dump Extract
+
+Extract relevant triples from DBPedia dumps.
+
+## Dumps
+
+[Latest releases of core data from en.wikipedia.org](https://databus.dbpedia.org/dbpedia/collections/latest-core) has dumps for various subsets of DBPedia.
+
+For example, the short abstracts are a 480.8 Mb download. We can then parse each line and extract the text for reuse.
+
---
+++ extract.php
@@ -0,0 +1,39 @@
+<?php
+
+$row_count = 1;
+
+$filename = "short-abstracts_lang=en.ttl";
+
+$headings = array('enwiki', '`key`', 'language', 'value');
+
+echo join("\t", $headings) . "\n";
+
+$file_handle = fopen($filename, "r");
+while (!feof($file_handle)) 
+{
+	$line = trim(fgets($file_handle));
+	
+	//echo $line . "\n";
+	
+	if (preg_match('/<http:\/\/dbpedia.org\/resource\/(?<enwiki>.*)> <http:\/\/www.w3.org\/2000\/01\/rdf-schema#comment>\s+"(?<value>.*)"@(?<language>[a-z]+)/', $line, $m))
+	{
+		//print_r($m);
+		
+		$row = array(
+			$m['enwiki'],
+			'comment',
+			$m['language'],
+			$m['value']
+		);
+		
+		echo join("\t", $row) . "\n";
+	}
+	
+	if ($row_count++ == 10)
+	{
+		exit();
+	}
+	
+}	
+
+
